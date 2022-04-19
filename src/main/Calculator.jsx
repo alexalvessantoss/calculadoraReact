517ae11b4f1153724e254fefcac36cb940e41de7
Alex Alves Santos <94087829+alexalvessantoss@users.noreply.github.com>
2022-04-18 23:24:37 -0300

    Removal do eval
    Removal do eval

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -32,11 +32,11 @@ export default class Calculator extends Component {
             const currentOperation = this.state.operation
 
             const values = [...this.state.values]
-            try{
+            /*try{
                 values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
             }catch(e){
                 values[0] = this.state.values[0]
-            }
+            }*/
             
             values[1] = 0
 
